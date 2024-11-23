fe1841e957f23a1e5ba3941eacfbb65784a0c393
Vickyvackydev <vickyvacky5@gmail.com>
2024-11-23 14:04:44 -0800

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
-import LexTechAi from "./lextech-ai";
 import Login from "./login";
 
 const inter = Inter({ subsets: ["latin"] });
